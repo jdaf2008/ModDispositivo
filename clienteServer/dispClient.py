7a0ce3a79a2e4e4dafcb5d5d88bf79df9b769977
Juan Arias <jdafcorp2008@gmail.com>
2018-04-06 16:07:47 -0500

    cambios en dispClient, se agregó los metodos de lectura de los sensores

--- clienteServer/dispClient.py
+++ clienteServer/dispClient.py
@@ -4,15 +4,24 @@ import httplib2
 import json
 import time
 import datetime
-
+import MCP342X, wind_direction, HTU21D, bmp085, tgs2600, ds18b20_therm
+import database
 
 URL_BASE = "http://ec2-54-202-254-90.us-west-2.compute.amazonaws.com:8080/"
 URL_API = "/backendAmbiente/webresources/generic/"
 URL_NAME_METHOD = "saveMedicion"
 
 
+def getSensorData():
 
-def main():
+	pressure = bmp085.BMP085()
+	temp_probe = ds18b20_therm.DS18B20()
+	air_qual = tgs2600.TGS2600(adc_channel = 0)
+	humidity = HTU21D.HTU21D()
+	wind_dir = wind_direction.wind_direction(adc_channel = 0, config_file="wind_direction.json")
+	interrupts = interrupt_client.interrupt_client(port = 49501)
+
+def requestPost():
 
 	""" Client RestFul """
 	httplib2.debuglevel = 0
@@ -21,20 +30,29 @@ def main():
 
 	url = URL_BASE + URL_API + URL_NAME_METHOD
 
+	
 	data = 	{
 				"idSensor":1,         
-				"TipoSensor":"co2",   
+				"TipoSensor":,   
 				"Fecha":"2018-04-05",
 				"datoObtenido":20.3,  
 				"Hora":"11:00:00"
 			}
 
 	headers = {'Content-Type': content_type_header}
+
 	print ("Posting %s" % data)
 
 	response, content = http.request(url, 'POST', json.dumps(data), headers=headers)
 	print(response)
 	print(content)
+
+
+
+def main():
+
+	getSensorData()
+	requestPost()
 
 
 if __name__ == '__main__':

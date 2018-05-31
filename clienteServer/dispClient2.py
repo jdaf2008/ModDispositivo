820b5a5d1ad146d649620dd32ebfa944044d5575
Juan Arias <jdafcorp2008@gmail.com>
2018-05-31 17:38:50 -0500

    version 2 dispoclient con validaciones de datos y manejo de errores

--- clienteServer/dispClient2.py
+++ clienteServer/dispClient2.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+# Funcion para el envio de variables de factores contaminantes al proyecto SIMONA
+# Version 2.0
+# Creado por Juan David Arias jdariasf@correo.udistrital.edu.co 31 de mayo de 2018
+
 import httplib2
 import json
 import time
@@ -12,40 +16,39 @@ import interrupt_client, MCP342X, wind_direction, HTU21D, bmp085, tgs2600, ds18b
 URL_BASE = "http://ec2-54-202-254-90.us-west-2.compute.amazonaws.com:8080/"
 URL_API = "/backendAmbiente/webresources/generic/"
 URL_NAME_METHOD = "saveMedicion"
-
+# dirJSON = "./ModDispositivo/clienteServer/testSensores.JSON"
+dirJSON = "testSensores.JSON"
 
 def getSensorData():
 
-        presion = Sensor("1","presion",bmp085.BMP085().get_pressure(),"null")
-        temp = Sensor("2","temperatura", ds18b20_therm.DS18B20().read_temp(),"null")
-        air = Sensor("3","calidadAire",tgs2600.TGS2600(adc_channel = 0).get_value(),"null")
-        humidity = Sensor("4","humedad",HTU21D.HTU21D().read_temperature(),"null")
-        metano = Sensor("5", "metano",getMetano(),"null")
-        noise = Sensor("6","ruido",getNoise(),"null")
-        air2 = Sensor("7","calidadAire2", getAirQ(),"null")
+        presion = Sensor("1","presion",bmp085.BMP085().get_pressure(),True)
+        temp = Sensor("2","temperatura", ds18b20_therm.DS18B20().read_temp(),True)
+        air = Sensor("3","calidadAire",tgs2600.TGS2600(adc_channel = 0).get_value(),True)
+        humidity = Sensor("4","humedad",HTU21D.HTU21D().read_temperature(),True)
+        metano = Sensor("5", "metano",getMetano(),True)
+        noise = Sensor("6","ruido",getNoise(),True)
+        air2 = Sensor("7","calidadAire2", getAirQ(),True)
         sensors = [presion,temp,air,humidity,metano,noise,air2]
-        requestPost(sensors)
+        requestPost(getState(sensors))
 
-def getState(sensors):
-
-    with open('testSensores.JSON') as f:
+def getState(sensors): 
+    with open(dirJSON) as f:
             data = json.load(f)
             for item in data:
                 for sensor in sensors:
                     if sensor.idSensor == item["_idSensor"]:
                         sensor.estado = item["_status"]
-                        print(item["_status"])
     return sensors
 
 def requestPost(sensors):
 
     for sensor in sensors:
-
+        
         now = datetime.datetime.now()
 
 	""" Client RestFul """
 	httplib2.debuglevel = 0
-	http = httplib2.Http() 
+	http = httplib2.Http(timeout=4) 
 	content_type_header = "application/json"
 
 	url = URL_BASE + URL_API + URL_NAME_METHOD
@@ -62,10 +65,19 @@ def requestPost(sensors):
 	headers = {'Content-Type': content_type_header}
 
 	print ("Posting %s" % data)
-
-	response, content = http.request(url, 'POST', json.dumps(data), headers=headers)
-	print(response)
-	print(content)
+        
+        if sensor.valor != "-":
+            if sensor.estado == True:
+                try:
+                    response, content = http.request(url, 'POST', json.dumps(data), headers=headers)
+                    print(response)
+                    print(content)
+                except:
+                    print("Error, no se pudo realizar la conexion con el servidor")
+            else:
+                print("estado del sensor false")
+        else:
+            print("valor del sensor no valido")
 
 
 
